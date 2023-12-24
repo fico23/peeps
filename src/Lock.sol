6a6a01e9a58e52f99c8fcccb7a6cf4f4b986dae5
Filip Lasic <filip.lasic89@gmail.com>
2023-12-24 11:55:02 +0100

    lock logic

--- src/Lock.sol
+++ src/Lock.sol
@@ -5,12 +5,18 @@ import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
 import {FixedPointMathLib} from "solady/utils/FixedPointMathLib.sol";
 
 contract Lock {
+    using FixedPointMathLib for uint256;
+
     uint256 internal constant LOCK_DURATION = 420 hours;
     address internal constant RAISE_GOAL = 420 ether;
     address internal immutable TOKEN;
 
     uint256 internal constant ETH_PER_TOKEN_OFFSET = 96;
+    uint256 internal constant ENDS_OFFSET = 224;
+    uint256 internal constant AMOUNT_OFFSET = 128;
+
     uint256 internal constant MASK_96 = 0xffffffffffffffffffffffff;
+    uint256 internal constant MASK_128 = 0xffffffffffffffffffffffffffffffff;
 
     // Bits Layout:
     // - [0..159]    `ethPerToken`
@@ -20,30 +26,62 @@ contract Lock {
     struct UserInfo {
         uint32 ends;
         uint96 amount;
-        uint128 lastEthPerToken;
+        uint128 ethDebt;
     }
 
-    mapping(address user => UserInfo details) private userInfo;
+    // Bits Layout:
+    // - [0..31]    `ends`
+    // - [32..127]  `amount`
+    // - [128..255]  `ethDebt`
+    mapping(address user => uint256 info) private userInfo;
 
     constructor(address token) {
-        TOKEN = ERC20(token);
+        TOKEN = token;
     }
 
     function recieve() external payable {
         (uint256 ethPerToken, uint256 tokenLocked) = _readPoolInfo();
-        
-        ethPerToken += msg.value.divWad()
+
+        ethPerToken += msg.value.divWad(tokenLocked);
+
+        _writePoolInfo(ethPerToken, tokenLocked);
     }
 
     function lock(uint256 amount, address recipient) external {
         SafeTransferLib.safeTransferFrom(TOKEN, msg.sender, address(this), amount);
 
+        (uint256 ethPerToken, uint256 tokenLocked) = _readPoolInfo();
+        (uint256 ends, uint256 lockedAmount, uint256 debt) = _readUserInfo(msg.sender);
 
+        uint256 newDebt = ethPerToken.mulWad(lockedAmount);
+        unchecked {
+            uint256 claimable = newDebt - debt;
+            if (claimable > 0) {
+                SafeTransferLib.safeTransferETH(msg.sender, claimable);
+            }
+
+            _writePoolInfo(ethPerToken, tokenLocked + amount);
+            _writeUserInfo(msg.sender, 0, lockedAmount + amount, newDebt);
+        }
     }
 
     function _readPoolInfo() internal view returns (uint256, uint256) {
         uint256 currPoolInfo = poolInfo;
 
         return (currPoolInfo >> ETH_PER_TOKEN_OFFSET, currPoolInfo & MASK_96);
+    }
+
+    function _writePoolInfo(uint256 ethPerToken, uint256 tokenLocked) internal {
+        poolInfo = tokenLocked & MASK_96 | ethPerToken << ETH_PER_TOKEN_OFFSET;
+    }
+
+    function _readUserInfo(address user) internal view returns (uint256, uint256, uint256) {
+        uint256 currUserInfo = userInfo[user];
+
+        return (currUserInfo >> ENDS_OFFSET, currUserInfo >> AMOUNT_OFFSET & MASK_96, currUserInfo & MASK_128);
+    }
+
+    function _writeUserInfo(address user, uint256 ends, uint256 amount, uint256 ethDebt) internal {
+        userInfo[user] = ends << ENDS_OFFSET | amount << AMOUNT_OFFSET | ethDebt;
     }
 }
